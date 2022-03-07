969547f49c7bf6d17b7c11b252d112bc86f6b6ed
daniele <d.malitesta@gmail.com>
2022-03-08 00:10:41 +0100

    Added normalization to textual feature extraction.

---
+++ src/extract_sentiment_reviews.py
@@ -0,0 +1,22 @@
+from transformers import pipeline
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import os
+
+dataset = 'baby'
+train_reviews = pd.read_csv(f'../data/{dataset}/train_reviews.txt', sep='\t', header=None)
+sentiment_pipeline = pipeline(task="sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
+model = list(sentiment_pipeline.model.children())[-3]
+model.eval()
+tokenizer = sentiment_pipeline.tokenizer
+
+if not os.path.exists(f'../data/{dataset}/reviews/'):
+    os.makedirs(f'../data/{dataset}/reviews/')
+with tqdm(total=len(train_reviews)) as t:
+    for idx, row in train_reviews.iterrows():
+        inputs = tokenizer.encode_plus(row[2], add_special_tokens=True, truncation=True, padding="max_length",
+                                       return_attention_mask=True, return_tensors="pt")
+        np.save(f'../data/{dataset}/reviews/{row[1]}_{row[0]}.npy',
+                model(**inputs).pooler_output.detach().cpu().numpy())
+        t.update()
