38951eb394e278e1b3e73e235da392ac3b317cb4
daniele <d.malitesta@gmail.com>
2022-03-08 11:03:43 +0100

    Added normalization to textual feature extraction.

--- src/extract_sentiment_reviews.py
+++ src/extract_sentiment_reviews.py
@@ -12,7 +12,9 @@ args = parser.parse_args()
 dataset = args.dataset
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 train_reviews = pd.read_csv(f'../data/{dataset}/train_reviews.txt', sep='\t', header=None)
+train_reviews.columns = ['user', 'item', 'review']
 sentiment_pipeline = pipeline(task="sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
+users_items_nan = train_reviews[train_reviews['review'].isna()][['user', 'item']]
 model = list(sentiment_pipeline.model.children())[-3]
 model.eval()
 model.to(device)
@@ -22,11 +24,12 @@ if not os.path.exists(f'../data/{dataset}/reviews/'):
     os.makedirs(f'../data/{dataset}/reviews/')
 with tqdm(total=len(train_reviews)) as t:
     for idx, row in train_reviews.iterrows():
-        try:
-            inputs = tokenizer.encode_plus(row[2], truncation=True, return_tensors="pt").to(device)
-            np.save(f'../data/{dataset}/reviews/{row[1]}_{row[0]}.npy',
-                    model(**inputs.to(device)).pooler_output.detach().cpu().numpy())
-        except:
-            print(f'user: {row[0]}, item: {row[1]}')
-            exit()
+        if len(users_items_nan[
+                   (users_items_nan['user'] == row['user']) & (users_items_nan['item'] == row['item'])]) > 0:
+            first_review = train_reviews[train_reviews['user'] == row['user']].dropna().iloc[0, 2]
+            inputs = tokenizer.encode_plus(first_review, truncation=True, return_tensors="pt").to(device)
+        else:
+            inputs = tokenizer.encode_plus(row['review'], truncation=True, return_tensors="pt").to(device)
+        np.save(f'../data/{dataset}/reviews/{row["item"]}_{row["user"]}.npy',
+                model(**inputs.to(device)).pooler_output.detach().cpu().numpy())
         t.update()
