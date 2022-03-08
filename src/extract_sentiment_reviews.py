3535e0382efd30837ea2bbdb86f61f78ec178a4d
daniele <d.malitesta@gmail.com>
2022-03-08 10:14:22 +0100

    Added normalization to textual feature extraction.

--- src/extract_sentiment_reviews.py
+++ src/extract_sentiment_reviews.py
@@ -1,4 +1,4 @@
-import torch.cuda
+import torch
 from transformers import pipeline
 from tqdm import tqdm
 import pandas as pd
@@ -10,15 +10,15 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 train_reviews = pd.read_csv(f'../data/{dataset}/train_reviews.txt', sep='\t', header=None)
 sentiment_pipeline = pipeline(task="sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
 model = list(sentiment_pipeline.model.children())[-3]
-model.to(device)
 model.eval()
-tokenizer = sentiment_pipeline.tokenizer
+model.to(device)
+tokenizer = sentiment_pipeline.tokenizer.to(device)
 
 if not os.path.exists(f'../data/{dataset}/reviews/'):
     os.makedirs(f'../data/{dataset}/reviews/')
 with tqdm(total=len(train_reviews)) as t:
     for idx, row in train_reviews.iterrows():
-        inputs = tokenizer.encode_plus(row[2], add_special_tokens=True, truncation=True, return_tensors="pt").to(device)
+        inputs = tokenizer.encode_plus(row[2], truncation=True, return_tensors="pt").to(device)
         np.save(f'../data/{dataset}/reviews/{row[1]}_{row[0]}.npy',
                 model(**inputs.to(device)).pooler_output.detach().cpu().numpy())
         t.update()
